e1464ec72bd3bfb55daa26e20bab130afb834e57
nirmal <kasungunathilaka1991@gmail.com>
2016-11-04 16:24:34 +0530

    - adding base activity - adding menu icon

--- app/src/main/java/com/kasungunathilaka/rentmanager/MainActivity.java
+++ app/src/main/java/com/kasungunathilaka/rentmanager/MainActivity.java
@@ -1,9 +1,6 @@
 package com.kasungunathilaka.rentmanager;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,7 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -71,17 +68,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_property) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_financial) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_miles) {
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
